573d802da1456b84dcb449e87af39f8bbb7bce71
Hassan Aijaz <sainbeloved@gmail.com>
2019-11-03 16:56:10 +0500

    Python Assignment 1

---
+++ Saylani_Assignment.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment
+
+# # Write a program to print the following string in a specific format.
+
+# In[35]:
+
+
+print("Twinkle, Twinkle, little star,\n"
+           "    How I wonder what you are!\n"
+               "\t Up above the world so high,\n"
+               "\t tlike a diamond in the sky.\n"
+      "Twinkle, Twinkle, little star,\n"
+           "      How I wonder what you are")
+
+
+# In[24]:
+
+
+version = float(input("Enter Python Version: Which you are using: "))
+print("Version: ",version)
+
+
+# # Write a python program to display the current date and time
+
+# In[25]:
+
+
+print("Current Date"+" "+"2/10/2019\n")
+print("Current Time"+" "+"10:10 AM")
+
+
+# # Write a python program which accepts the radius of a circle from the user and compute the area
+
+# In[30]:
+
+
+print("Calcuate the area of circle\n")
+radius = float(input("Enter radius : "))
+area = (3.14*radius*radius)
+print("Area of circle is = ", area)
+
+
+# # Write a python program which accepts the user's first and last name and print them in reverse order with a space between them
+
+# In[33]:
+
+
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+print(last_name+ " "+first_name)
+
+
+# # Write a python program which takes two inputs from user and print them in addition
+
+# In[34]:
+
+
+number1 = int(input("Enter Number 1: "))
+number2 = int(input("Enter Number 2: "))
+print("Addition is = ", number1+number2)
+
+
+# # That's All for Assignment, Thank you Saylani. 
